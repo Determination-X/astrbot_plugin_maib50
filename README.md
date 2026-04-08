cbc81c135ffd44dfef6f7bc20be298d79174062f
Determination-X <233097716+Determination-X@users.noreply.github.com>
2026-04-08 16:49:08 +0800

    update

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # astrbot-plugin-helloworld
 
-AstrBot 插件模板 / A template plugin for AstrBot plugin feature
+AstrBot B50插件 / An AstrBot plugin for B50
 
-> [!NOTE]
-> This repo is just a template of [AstrBot](https://github.com/AstrBotDevs/AstrBot) Plugin.
-> 
 > [AstrBot](https://github.com/AstrBotDevs/AstrBot) is an agentic assistant for both personal and group conversations. It can be deployed across dozens of mainstream instant messaging platforms, including QQ, Telegram, Feishu, DingTalk, Slack, LINE, Discord, Matrix, etc. In addition, it provides a reliable and extensible conversational AI infrastructure for individuals, developers, and teams. Whether you need a personal AI companion, an intelligent customer support agent, an automation assistant, or an enterprise knowledge base, AstrBot enables you to quickly build AI applications directly within your existing messaging workflows.
 
 # Supports
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.event import filter, AstrMessageEvent
+import astrbot.api.message_components as Comp
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+plugin_name = "astrbot_plugin_maib50"
+help_text = """
+/mai可用指令: 
+├─/mai b50
+├─/mai bind INT <好友码>
+│ /mai bind CN (开发中)
+│ /mai bind JP (暫定)
+├─/mai help
+└─/mai search <关键词>
+
+服务器参数说明:
+INT: 国际服 CN: 国服 RIN: 日服 MUNET: MuNET"""
+
+@register("maib50", "Determination_X", "Maib50 国际服插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
@@ -20,5 +34,58 @@ class MyPlugin(Star):
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
 
+    @filter.command_group("mai")
+    async def mai(self, event: AstrMessageEvent):
+        yield event.plain_result(help_text)
+
+    @mai.command("help")
+    async def mai_help(self, event: AstrMessageEvent):
+        yield event.plain_result(help_text)
+
+    @mai.command("bind")
+    async def mai_bind(self, event: AstrMessageEvent, server: str, friend_code: str):
+        if server == "help" and friend_code == "":
+            yield event.plain_result("""
+服务器参数说明:
+INT int 国际服 國際服 International
+CN  cn  国服 國服 China
+JP  jp  日服  Japan JPN jpn
+RIN rin Rin服 RinNET
+MUNET munet MuNET""")
+            return
+        if server not in ["INT", "CN", "JP", "RIN", "MUNET",
+                          "int", "cn", "jp", "rin", "munet", 
+                          "国际服", "国服", "日服", "Rin服",
+                          "國際服", "國服",
+                          "International", "China", "Japan", "RinNET", "MuNET",
+                          "JPN", "jpn"]:
+            yield event.plain_result("服务器输错了喵！请使用 INT、CN、RIN 或 MUNET 作为服务器参数")
+            return
+        if server != "INT" and server != "int" and server != "国际服" and server != "International":
+            yield event.plain_result(f"{server} 的绑定功能正在开发喵~为什么不去找开发者催更呢w？")
+            return
+        if len(event.message_str.split()) < 3:
+            yield event.plain_result("参数错误！请使用 /mai bind <服务器> <好友码> 的格式进行绑定喵")
+            return
+        if len(friend_code) != 13 or not friend_code.isdigit():
+            yield event.plain_result("好友码输错了喵！好友码应该是13位数字")
+            return
+
+
+    @mai.command("b50")
+    async def mai_b50(self, event: AstrMessageEvent):
+        """查询maimai b50数据"""
+        # code for login and data fetching here
+        # code for image generation here
+        chain= [
+            Comp.At(qq=event.get_sender_id()),
+            Comp.Plain(" 你的B50来了喵~"),
+            Comp.Image.fromFileSystem(f"data/plugin_data/{plugin_name}/b50_image/{event.get_sender_id()}.jpg")
+        ]
+        yield event.chain_result(chain)
+        
+    # @mai.command("search")
+    
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        pass
