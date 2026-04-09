54f8e16505949017adf1637c3d79b39240524d03
Determination-X <233097716+Determination-X@users.noreply.github.com>
2026-04-09 12:40:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from astrbot.api.event import filter, AstrMessageEvent
 import astrbot.api.message_components as Comp
 from astrbot.api.star import Context, Star, register
-from astrbot.api import logger#, AstrBotConfig
+from astrbot.api import logger, AstrBotConfig
 import aiohttp
 
 plugin_name = "astrbot_plugin_maib50"
