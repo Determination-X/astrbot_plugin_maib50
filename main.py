5946611d935e8090c38877dd340659c664d2e632
Determination-X <233097716+Determination-X@users.noreply.github.com>
2026-05-14 11:16:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -607,7 +607,7 @@ class MaiPlugin(Star):
         # 4. Options for rendering
         options = {
             "viewport": {"width": 1200, "height": 700},
-            #"device_scale_factor": 2,
+            "device_scale_factor": 4,
             "full_page": False,  # To match the container screenshot
             "scale": "css"
         }
